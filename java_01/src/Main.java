7f022ea852eb81c888090bcdff90d33ab634d76a
Bald <heitoraugusto.fiapschool@gmail.com>
2026-02-03 09:02:47 -0300

    Add files via upload

---
+++ java_01/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Hello world!"));
+
+    System.out.println("Hello World");
+
+    int idade = 20;
+    int quantidadePessoas = 7;
+    float peso = 69.7F;
+    double altura = 1.70;
+    String nome = "Heitor";
+    char sexo = 'M';
+
+    System.out.println("Meu nome é "+nome+" tenho "+idade+" anos de idade, sou do sexo "+sexo+", tenho "+altura+" de altura meu peso é "+peso+" e estou participando de uma entrevista com "+quantidadePessoas+" Sparticipantes.");
+}
